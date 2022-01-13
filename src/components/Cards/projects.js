84d82c630b09a970a8259e2439c553a5ce80204c
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-01-13 22:27:20 +0545

    Updating repo.

--- src/components/Cards/projects.js
+++ src/components/Cards/projects.js
@@ -16,10 +16,10 @@ export const projects = [{
     },
     {
         id: 3,
-        name: 'Mero Projects',
-        info: 'A website which contains mini-projects that are created while doing a self-study on reactjs.',
-        img: './images/projects/mero-projects.png',
-        github: 'https://github.com/bibashmgr/my-projects',
-        demo: 'https://mero-projects.netlify.app/',
+        name: 'EShop',
+        info: 'An ecommerce site for digital products which is created using reactjs & @reduxjs/toolkit.',
+        img: './images/projects/eshop.png',
+        github: 'https://github.com/bibashmgr/EShop',
+        demo: 'https://eshop-site.netlify.app/',
     },
 ];
